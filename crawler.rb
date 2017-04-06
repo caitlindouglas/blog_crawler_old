461d7e0decc5e7385c91865949ed2c48be75187c
Caitlin Goldman <cgoldman@gapintelligence.com>
2017-04-05 21:20:14 -0700

    Add all the crawler dependencies

--- crawler.rb
+++ crawler.rb
@@ -1,11 +1,15 @@
 require 'nokogiri'
 require 'mechanize'
+require 'mechanize_adapter'
+require 'active_support/core_ext'
+require 'pry-byebug'
 
 class Crawler
+	include MechanizeAdapter
+
 	def crawl(url)
 		agent = Mechanize.new
 		agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
 		blog_page = agent.get(url)
 
 		puts blog_page.title
