d0d08aadcd70e5ccb4172ba799d45c2dffa4ffd2
Caitlin Goldman <cgoldman@gapintelligence.com>
2017-04-12 11:00:01 -0700

    Real pagination and parsing the title of each blog

--- crawler.rb
+++ crawler.rb
@@ -2,25 +2,29 @@ require 'nokogiri'
 require 'mechanize'
 require 'mechanize_adapter'
 require 'active_support/core_ext'
+require 'pry'
 
 class Crawler
 	include MechanizeAdapter
 
 	def crawl(url)
-		blog_page = agent.get(url)
+		link = url
 
-		current_page = 1
-		while current_page < 5
-			puts blog_page.extract('.b-post a')
+		begin
+			blog_page = agent.get(link)
+			
+			parse_blogs(blog_page)
 
-			next_page_url = blog_page.extract('a.older_posts', attr: :href)
-			blog_page = agent.get(next_page_url)
-
-			current_page += 1
-		end
+			link = blog_page.extract('a.older_posts', attr: :href)
+		end while link
 	end
 
 	def parse_blogs(page)
+		page.extract_all('.b-post a.btn.btn-lg__trans--color3', attr: :href).each do |read_more_url|
+			read_more_page = agent.get(read_more_url)
+
+			puts read_more_page.extract('.jumbotron-sub__post-title')
+		end
 	end
 
 	def agent
